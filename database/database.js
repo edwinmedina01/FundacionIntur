9f685c435cd7f0c970ced7f73026da7344632296
Arle Andino <Arle Andino@DESKTOP-R70DFNE>
2025-01-29 09:43:45 -0600

    mysql2 vercer

--- database/database.js
+++ database/database.js
@@ -1,5 +1,14 @@
 // database.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+try {
+    require.resolve("mysql2");
+    console.log("✅ mysql2 está instalado en Vercel");
+  } catch (e) {
+    console.error("❌ mysql2 NO está instalado en Vercel");
+  }
+  
+
 
 //Crea una nueva instancia de Sequelize
 // const sequelize = new Sequelize('sql3744863', 'sql3744863', 'LIi2cJvQgS' , {
@@ -11,10 +20,23 @@ const { Sequelize } = require('sequelize');
 
 
 // Crea una nueva instancia de Sequelize
- const sequelize = new Sequelize('gestion_academica_db', 'root', 'soren1' , {
-     host: 'localhost',
-     dialect: 'mysql', // Cambia a 'postgres', 'sqlite', etc., si es necesario
-  });
+//  const sequelize = new Sequelize('gestion_academica_db', 'root', 'soren1' , {
+//      host: 'localhost',
+//      dialect: 'mysql', // Cambia a 'postgres', 'sqlite', etc., si es necesario
+//   });
+
+
+//   const sequelize = new Sequelize('gestion_academica_db', 'root', 'JjGbmRqkujBejmrjINqrYfCvzVwUxFdS' , {
+//     host: 'autorack.proxy.rlwy.net',
+//     dialect: 'mysql', // Cambia a 'postgres', 'sqlite', etc., si es necesario
+//  });
+
+ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    logging: false,
+});
 
 
 // Verifica la conexión
